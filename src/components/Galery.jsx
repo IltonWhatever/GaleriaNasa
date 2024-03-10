be7ca9d8f7681419697a5f0673d59b14606e4cf1
IltonWhatever <101358506+IltonWhatever@users.noreply.github.com>
2024-03-10 12:40:36 -0300

    Mudança da dinamica para trabalhar com coleção de Jsons

--- src/components/Galery.jsx
+++ src/components/Galery.jsx
@@ -1,59 +1,83 @@
-// Galery.jsx
 import { BsArrowLeftSquareFill, BsArrowRightSquareFill } from "react-icons/bs";
 import "../css/galery.css";
 import React, { useState, useEffect } from "react";
 
 function Galery({ dataProvider }) {
-  const [displayImage, setDisplayImage] = useState('../../public/default.jpg');
-  const [informations, setInformations] = useState(" ");
-  const [carrocel, setCarrocel] = useState([{img:null, exp: null}]);
+  // Imagem da Galeria.
+  const [displayImage, setDisplayImage] = useState('/default.jpg');
+  // Informações abaixo da galeria.
+  const [currentInformation, setCurrentInformation] = useState("");
+  // Informação do Titulo.
+  const [title, setTitle] = useState('Nasa Galery');
+  // Variavel onde fica o buff dos requests.
+  const [carrocel, setCarrocel] = useState([]);
+  // Variavel de indice para saber onde esta nas imagens do carrocel.
   const [indice, setIndice] = useState(0);
+
+
+  useEffect(() => {
+    addCarrocel(); // Carregar as primeiras imagens ao montar o componente
+  }, []);
 
   const addCarrocel = async () => {
     try {
       // Coletando dados da API com base na função recebida.
       const rawData = await dataProvider.fetchData();
+
       // Formatando dados brutos com base na função recebida.
       const formattedData = await dataProvider.formatData(rawData);
 
-      // Adicionando a Lista do Carrocel.
-      setCarrocel(prevCarrocel => [...prevCarrocel, formattedData]);
+      // Adicionando item a item da coleção Json para o Carrocel
+      formattedData.forEach((item) => {
+        setCarrocel((prevCarrocel) => [...prevCarrocel, item]);
+      });
+
+      // Alterando a Imagem.
       await setDisplayImage(formattedData.img);
-      setInformations(formattedData.exp);
+      // Alterando o texto de informação.
+      await setCurrentInformation(formattedData.exp);
+      // Alterando o Titulo.
+      await setTitle(formattedData.title);
+
+
     } catch (error) {
       console.error('Erro ao adicionar ao carrossel:', error);
     }
   };
 
-  const moveInCarrocel = async (routeType, direction) => {
-    switch(carrocel.length){
-      case (carrocel.length === 1):{
-        await addCarrocel();
-        setIndice(prevIndice => prevIndice + 1);
-        break;}
+  const goToNextSlide = () => {
+    if (indice === carrocel.length - 1) {
+      addCarrocel();
+      setIndice(indice + 1); // Avança para o próximo slide
+    } else {
+      setIndice((prevIndex) => prevIndex + 1);
     }
-
-    if (carrocel) {
-      await addCarrocel();
-    } else if (direction === 'right') {
-      if (carrocel.length === 0) {
-        await addCarrocel();
-        setIndice(prevIndice => prevIndice + 1)
-      }
-    }
-  }
+  };
+  
+  const goToPrevSlide = () => {
+    setIndice((prevIndex) =>
+      prevIndex === 0 ? carrocel.length - 1 : prevIndex - 1
+    );
+  };  
+  
+  // Responsavel por atualizar tudo com base no carrocel e indice.
+  useEffect(() => {
+    setCurrentInformation(carrocel[indice]?.exp || "");
+    setTitle(carrocel[indice]?.tit || "");
+  }, [indice, carrocel]);
 
   return (
     <div className="master">
+      <div className="title">{title}</div>
       <div className="center">
-        <BsArrowLeftSquareFill className="buttonLeft" onClick={() => moveInCarrocel('teste', 'left')} />
+        <BsArrowLeftSquareFill className="buttonLeft" onClick={() => goToPrevSlide()} />
         <div className="display">
-          <img src={displayImage} className="imageDisplay" alt="displayed" />
+          <img src={carrocel[indice]?.img || '/default.jpg'} className="imageDisplay" alt="displayed" />
         </div>
-        <BsArrowRightSquareFill className="buttonRight" onClick={() => moveInCarrocel('teste', 'right')} />
+        <BsArrowRightSquareFill className="buttonRight" onClick={() => goToNextSlide()} />
       </div>
       <div className="informations">
-        <p className="textInformations">{informations}</p>
+        <p className="textInformations">{currentInformation}</p>
       </div>
     </div>
   );
--- src/util/UniverseDataFormatter.js
+++ src/util/UniverseDataFormatter.js
@@ -3,12 +3,18 @@ import DataFormatter from './DataFormatter';
 class UniverseDataFormatter extends DataFormatter {
   formatData(rawData) {
     // Formatação
-    const formatedJson = {
-      "img": rawData[0].hdurl,
-      "exp": rawData[0].explanation
-    }
+    // const formatedJson = {
+    //   "img": rawData[0].hdurl,
+    //   "exp": rawData[0].explanation
+    // }
+    console.log('Foi feito Request');
 
-    return formatedJson;
+    return rawData.map(item =>({
+      img: item.hdurl,
+      exp: item.explanation,
+      tit: item.title,
+      dat: item.date
+    }));
   }
 }
 
